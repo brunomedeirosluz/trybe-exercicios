e169240ef117993dceca2bafeaf3cff87eeb2aa4
Bruno Medeiros <brunoml95@hotmail.com>
2023-05-02 18:53:12 -0300

    exercicios

---
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,128 @@
+// // exercicio 1  
+
+
+// let a = 10; 
+// let b = 5;
+
+
+// console.log (a + b);
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+
+// // exercicio 2 
+
+
+// let c = 50;
+// let d = 60;
+
+// if (c > d){
+// console.log('c é maior que d')
+// }
+// else {
+// console.log('d é maior que c')
+// }
+
+// // exercicio 3
+// let e = 80;
+// let f = 60; 
+// let g = 90;
+
+
+// if (e > f && e > g){
+//     console.log('e é o maior numero')
+// }
+// else if (f > e && f > g){
+//     console.log('f é o maior numero')
+// }
+// else {
+//     console.log('g é o maior numero')
+// }
+
+// exercicio 4
+
+// let parametro = 10;
+
+// if (parametro > 0){
+//     console.log('positive');
+// }else if (parametro < 0){
+//     console.log('negative')
+// }else {
+//     console.log('zero');
+// }
+
+// exercicio 5
+
+// let angulo1 = 60;
+// let angulo2 = 60;
+// let angulo3 = 60;
+
+// let somaTodosAngulos = angulo1 + angulo2 + angulo3; 
+
+// let todosAngulosPositivo;
+
+// if (todosAngulosPositivo = angulo1 > 0 && angulo2 > 0 && angulo3 > 0){
+//   if(somaTodosAngulos === 180){
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   };
+// } else {
+//     console.log('Erro: ângulo inválido');
+// }
+  
+// exercicio 6
+
+// let pecaXadrez = 'DAMA';
+
+// switch (pecaXadrez.toLowerCase()){
+//     case 'peao':
+//     console.log ('Peão -> Movimenta uma casa pra frente.');
+//     break;
+
+//     case 'cavalo':
+//     console.log ('Cavalo -> Movimenta em formato de L.');
+//     break;
+
+//     case 'rei':
+//     console.log ('Rei -> Pode mover-se em qualquer direção, porém apenas uma casa por vez.');
+//     break;
+
+//     case 'dama':
+//     console.log ('Dama -> Pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres.');
+//     break
+
+//     case 'torre':
+//     console.log ('Torre -> Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.');
+//     break
+
+//     case 'bispo':
+//     console.log('Bispo -> Move-se na diagonal, quantas casas quiser.');
+
+//     default:
+//     console.log('Peça inválida!!')
+// }
+
+// exercicio 7 
+
+// let nota = 70;
+
+// if (nota < 0 || nota > 100){
+//     console.log('ERRO!!!');
+// }else if (nota >= 90){
+//     console.log('A');
+// }else if (nota >= 80){
+//     console.log('B');
+// }else if (nota >= 70){
+//     console.log('C');
+// }else if (nota >= 60){
+//     console.log('D');
+// }else if (nota >= 50){
+//     console.log('E');
+// }else{
+//     console.log('F');
+// }
+
+
