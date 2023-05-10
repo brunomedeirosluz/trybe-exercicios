24731cd0756d0a518d0e4df3018091e2209e3e3a
Bruno Medeiros <brunoml95@hotmail.com>
2023-05-10 19:38:01 -0300

    exercicios

--- 1-fundamentos/secao-03-introducao-a-javascript/dia-05-JavaScript-ES6/exercise.js
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-05-JavaScript-ES6/exercise.js
@@ -167,5 +167,19 @@ const substituaX = (nome) => {
     return fraseArray.join(' ');   
 }
 
-console.log(substituaX('Bruno'));
+// console.log(substituaX('Bruno'));
 
+ const minhasSkills = (func) => {
+    const skills = ['Java Script','HTML','CSS'];
+    let resultado = `
+    ${func}
+    Minhas trê principais habilidades são:`;
+    for (let index = 0; index < skills.length; index +=1){
+    resultado = ` ${resultado}
+    - ${skills[index]}`;
+    }
+    return resultado
+ }
+
+
+console.log(minhasSkills(substituaX('Bruno')));
