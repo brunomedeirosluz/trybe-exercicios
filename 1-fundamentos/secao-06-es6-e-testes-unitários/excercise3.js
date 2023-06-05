3f86d6d8410876c59f96c6850500e6959286ee14
Bruno Medeiros <brunoml95@hotmail.com>
2023-06-05 13:36:27 -0300

    pastas

---
+++ 1-fundamentos/secao-06-es6-e-testes-unitários/excercise3.js
@@ -0,0 +1,20 @@
+const verifyRegister = (name, age) => {
+    if (!name || !age){
+        throw new Error('Todas as informações são necessárias');
+    }
+    if (name && age < 18){
+        throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+    }
+}
+
+
+const studentRegister = (name, age) => {
+    try {
+        verifyRegister(name, age);
+        return `${name}, seja bem-vindo(a) à AuTrybe!' `
+    }catch (error){
+        return error.message
+    }
+  }
+
+console.log(studentRegister('Bruno',18));
