b47a915de0451c7e2c42f56377920a5b98941f82
Bruno Medeiros <brunoml95@hotmail.com>
2023-10-09 20:49:01 -0300

    exercicio

--- 1-fundamentos/secao-07-Higher-Order-Functions/dia-05-javascript-es6/exercise5.js
+++ 1-fundamentos/secao-07-Higher-Order-Functions/dia-05-javascript-es6/exercise5.js
@@ -1,10 +1,42 @@
-const moreStudents = [
-    'Chris', 
-    ['Ahmad', 'Antigoni'], 
-    ['Toby', 'Sam']
+// const moreStudents = [
+//     'Chris', 
+//     ['Ahmad', 'Antigoni'], 
+//     ['Toby', 'Sam']
+//   ];
+
+//   // Escreva seu código aqui
+//   const [student1,[student2,student3], [student4,student5]] = moreStudents;
+
+// console.log(student1, student2, student3, student4, student5);
+
+
+const cart = [
+    {
+      name: "laptop",
+      price: 1000,
+      quantity: 2,
+      id: 3,
+    },
+    {
+      name: "desktop",
+      price: 1500,
+      quantity: 1,
+      id: 2,
+    },
+    {
+      name: "phone",
+      price: 500,
+      quantity: 3,
+      id: 1,
+    },
   ];
-
-  // Escreva seu código aqui
-  const [student1,[student2,student3], [student4,student5]] = moreStudents;
-
-console.log(student1, student2, student3, student4, student5);
+  
+  // escreva seu código aqui
+  const valorInicial = 0;
+  const totalCart = cart.reduce((accumulator, product) => accumulator + product.price * product.quantity, valorInicial);
+  
+  
+  // fim do código
+  
+  console.log(totalCart);
+  
